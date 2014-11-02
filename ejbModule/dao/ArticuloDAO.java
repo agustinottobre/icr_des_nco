dd694faa48d3eec6ce0565ad72005a463165b630
echarly02 <echarly02@gmail.com>
2014-11-02 15:12:41 -0300

    carlos

--- ejbModule/dao/ArticuloDAO.java
+++ ejbModule/dao/ArticuloDAO.java
@@ -1,5 +1,21 @@
 package dao;
 
+
+import javax.persistence.PersistenceContext;
+import javax.persistence.EntityManager;
+
+import despacho.dominio.Articulo;
+
 public class ArticuloDAO {
+	
+	@PersistenceContext(unitName ="JPADB")
+	private EntityManager em;
+	
+	public void guardarArticulo(Articulo articulo){
+		em.persist(articulo);
+	}
+	
+	
+	
 	
 }
