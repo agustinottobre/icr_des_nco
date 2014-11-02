c8a8e83b48ed7001359c91b10e446b5f2e18f662
echarly02 <echarly02@gmail.com>
2014-11-02 16:49:39 -0300

    Revert "Cambios de pablo incluidos"
    This reverts commit 0f9cb2f736288e01147b873960b5a38a04e6b94b.

--- ejbModule/despacho/ejb/AdministradorOrdenesDespachoBean.java
+++ ejbModule/despacho/ejb/AdministradorOrdenesDespachoBean.java
@@ -13,7 +13,6 @@ import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
 
 import despacho.dominio.Articulo;
 import despacho.dominio.ItemOrdenDespacho;
@@ -21,6 +20,7 @@ import despacho.dominio.OrdenDespacho;
 import despacho.dominio.OrdenVenta;
 import despacho.dominio.Portal;
 import despacho.ejb.interfaces.remotas.AdministradorOrdenesDespacho;
+
 import dto.ItemOrdenDespachoDTO;
 
 //import despacho.ws.servicios.consumidos.ServidorEstadoEntregaBean;
@@ -46,82 +46,52 @@ public class AdministradorOrdenesDespachoBean implements AdministradorOrdenesDes
 	@Override
 	public OrdenDespachoDTO altaOrdenDespacho(OrdenDespachoDTO ordenDespachoDTO) {
 		
-		//Buscar el objeto en la base de datos.
-		OrdenDespachoDTO ordenBaseDatos;
-		ordenBaseDatos = this.buscarOrdenDespacho(String.valueOf(ordenDespachoDTO.getIdOrdenDespacho()));
+		Portal portal = new Portal();
+		portal.setIdPortal(ordenDespachoDTO.getOrdenVenta().getPortal().getIdPortal());
+		portal.setDescripcion(ordenDespachoDTO.getOrdenVenta().getPortal().getDescripcion());
+	
+		OrdenVenta ordenVenta = new OrdenVenta();
+		ordenVenta.setIdOrdenVenta(ordenDespachoDTO.getOrdenVenta().getIdOrdenVenta());
+		ordenVenta.setPortal(portal);
 		
-		//Si la orden existe en la Base de Datos muestro un mensaje
-		if (ordenBaseDatos != null) {
-			System.out.println("Ya existe una Orden de Despacho con el mismo ID");
-			return null;
-			
-		} else {
-			System.out.println("Se da de alta una Orden de Despacho con el ID: " + String.valueOf(ordenDespachoDTO.getIdOrdenDespacho()));
-		
-			Portal portal = new Portal();
-			portal.setIdPortal(ordenDespachoDTO.getOrdenVenta().getPortal().getIdPortal());
-			portal.setDescripcion(ordenDespachoDTO.getOrdenVenta().getPortal().getDescripcion());
-		
-			OrdenVenta ordenVenta = new OrdenVenta();
-			ordenVenta.setIdOrdenVenta(ordenDespachoDTO.getOrdenVenta().getIdOrdenVenta());
-			ordenVenta.setPortal(portal);
-			
-			OrdenDespacho ordenDespacho = new OrdenDespacho();
-			ordenDespacho.setIdOrdenDespacho(ordenDespachoDTO.getIdOrdenDespacho());
-			ordenDespacho.setEstadoOrden(ordenDespachoDTO.getEstadoOrden());
-			DateFormat df = new SimpleDateFormat("dd-MMM-yyyy");
-			try {
-				ordenDespacho.setFechaRecepcion(df.parse(ordenDespachoDTO.getFechaRecepcion()));
-			} catch (ParseException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		OrdenDespacho ordenDespacho = new OrdenDespacho();
+		ordenDespacho.setIdOrdenDespacho(ordenDespachoDTO.getIdOrdenDespacho());
+		ordenDespacho.setEstadoOrden(ordenDespachoDTO.getEstadoOrden());
+		DateFormat df = new SimpleDateFormat("dd-MMM-yyyy");
+		try {
+			ordenDespacho.setFechaRecepcion(df.parse(ordenDespachoDTO.getFechaRecepcion()));
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
-			ItemOrdenDespacho itemOD;
-			List<ItemOrdenDespacho> items = new ArrayList<ItemOrdenDespacho>();
-			Articulo articulo;
-			for (ItemOrdenDespachoDTO item : ordenDespachoDTO.getItems())
-			{
-				
-				articulo = new Articulo();
-				articulo.setIdArticulo(item.getArticulo().getIdArticulo());
-				
-				itemOD = new ItemOrdenDespacho();
-				itemOD.setArticulo(articulo);
-				itemOD.setCantidad(item.getCantidad());
-				itemOD.setEstadoItems(item.getEstadoItems());
-				
-				items.add(itemOD);
-			}
-			ordenDespacho.setItems(items);
+		
+		ItemOrdenDespacho itemOD;
+		List<ItemOrdenDespacho> items = new ArrayList<ItemOrdenDespacho>();
+		Articulo articulo;
+		for (ItemOrdenDespachoDTO item : ordenDespachoDTO.getItems())
+		{
 			
-			em.persist(ordenDespacho);
+			articulo = new Articulo();
+			articulo.setIdArticulo(item.getArticulo().getIdArticulo());
+			
+			itemOD = new ItemOrdenDespacho();
+			itemOD.setArticulo(articulo);
+			itemOD.setCantidad(item.getCantidad());
+			itemOD.setEstadoItems(item.getEstadoItems());
+			
+			items.add(itemOD);
+		}
+		ordenDespacho.setItems(items);
 		
-//			FALTA VERIFICAR SI SE HACE BIEN EL ALTA Y DEVOLVER lo que corresponda
+		em.persist(ordenDespacho);
+		
+		System.out.println("## altaOrdenDespacho SIN IMPLEMENTACION" );
 
-			return ordenDespachoDTO;	
-		
-		}	
-		
+//		this.notificarEntregaDespacho(777);
+
+		return null;
 	}
-
-
-
-	@Override
-	public OrdenDespachoDTO buscarOrdenDespacho(String idOrdenDespacho) {
-		// TODO Auto-generated method stub
-		
-		Query q = em.createQuery("SELECT FROM OrdenesDespacho o WHERE o.idOrdenDespacho = :id");
-				q.setParameter("id", idOrdenDespacho);
-				
-		OrdenDespacho ordenDespacho = (OrdenDespacho)q.getSingleResult();
-	
-		return ordenDespacho.getDTO();
-	}
-	
-	
-	
-	
 	
 	/*@Override
 	public String notificarEntregaDespacho (int idOrdenDespacho){
--- ejbModule/despacho/ejb/AdministradorSolicitudesArticuloBean.java
+++ ejbModule/despacho/ejb/AdministradorSolicitudesArticuloBean.java
@@ -1,23 +1,10 @@
 package despacho.ejb;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
 
-import despacho.dominio.Articulo;
-import despacho.dominio.ItemOrdenDespacho;
-import despacho.dominio.ItemSolicitudArticulo;
-import despacho.dominio.OrdenDespacho;
-import despacho.dominio.SolicitudArticulo;
 import despacho.ejb.interfaces.remotas.AdministradorSolicitudesArticulo;
 import dto.ArticuloDTO;
-import dto.ItemOrdenDespachoDTO;
-import dto.ItemSolicitudArticuloDTO;
 import dto.SolicitudArticuloDTO;
 
 /**
@@ -27,55 +14,20 @@ import dto.SolicitudArticuloDTO;
 @LocalBean
 public class AdministradorSolicitudesArticuloBean implements AdministradorSolicitudesArticulo {
 
-    @PersistenceContext(unitName = "JPADB")
-    private EntityManager em;
-    
+    /**
+     * Default constructor. 
+     */
     public AdministradorSolicitudesArticuloBean() {}
-
-	@Override
-	public SolicitudArticuloDTO altaSolicitudArticulo(SolicitudArticuloDTO solicitudArticuloDTO) {
-		// TODO Auto-generated method stub
-		SolicitudArticulo solicitudArticulo = new SolicitudArticulo();
-		solicitudArticulo.setEstadoSolicitud(solicitudArticuloDTO.getEstadoSolicitud());
-		solicitudArticulo.setIdDepostio(solicitudArticuloDTO.getIdDepostio());
-		solicitudArticulo.setIdSolicitud(solicitudArticuloDTO.getIdSolicitud());
-		
-		ItemSolicitudArticulo itemSA;
-		List<ItemSolicitudArticulo> items = new ArrayList<ItemSolicitudArticulo>();
-		Articulo articulo;
-		for (ItemSolicitudArticuloDTO item : solicitudArticuloDTO.getItems())
-		{
-			
-			articulo = new Articulo();
-			articulo.setIdArticulo(item.getArticulo().getIdArticulo());
-			
-			itemSA = new ItemSolicitudArticulo();
-			itemSA.setArticulo(articulo);
-			itemSA.setCantidad(item.getCantidad());
-			
-			items.add(itemSA);
-		}
-		solicitudArticulo.setItems(items);
-		
-		em.persist(solicitudArticulo);
-		
-//		FALTA VERIFICAR SI SE HACE BIEN EL ALTA Y DEVOLVER EL DTO EN CADA CASO
-		return solicitudArticuloDTO;
-	}
-
-	@Override
-	public SolicitudArticuloDTO buscarSolicitudArticulo(
-			String idSolicitudArticulo) {
-		// TODO Auto-generated method stub
-
-		Query q = em.createQuery("SELECT FROM SolicitudesArticulo sol WHERE sol.idSolicitudArticulo = :id");
-		q.setParameter("id", idSolicitudArticulo);
-		
-		SolicitudArticulo solicitud = (SolicitudArticulo)q.getSingleResult();
-
-		return solicitud.getDTO();
-	}
     
+    public long obtenerCantidadSolicitudesArticulo(){
+    	return 0;
+    }
 
+	@Override
+	public SolicitudArticuloDTO nuevaSolicitud(ArticuloDTO articulo,
+			int cantidad) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
