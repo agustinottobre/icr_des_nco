716ae4a1b2ffa53ab7c706bf61ea050681c9d96d
Agustin Ottobre <agustin.ottobre@gmail.com>
2014-11-05 21:48:53 -0300

    tests + servicio rest para deposito

--- ejbModule/despacho/ejb/AdministradorArticulosBean.java
+++ ejbModule/despacho/ejb/AdministradorArticulosBean.java
@@ -1,22 +1,16 @@
 package despacho.ejb;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.ejb.EJB;
 import javax.ejb.LocalBean;
 import javax.ejb.Startup;
 import javax.ejb.Stateless;
-import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import despacho.dominio.Articulo;
 import despacho.ejb.interfaces.remotas.AdministradorArticulos;
-import despacho.ejb.interfaces.remotas.ClienteJmsParaDeposito;
 import dto.ArticuloDTO;
-import dto.ItemSolicitudArticuloDTO;
-import dto.SolicitudArticuloDTO;
 
 /**
  * Session Bean implementation class AdministradorArticulosBean
@@ -31,31 +25,11 @@ public class AdministradorArticulosBean implements AdministradorArticulos{
      */
 	@PersistenceContext(unitName = "JPADB")
 	private EntityManager em;
-	
-	@EJB
-	private ClienteJmsParaDeposito clienteJmsParaDeposito;
 		
     //--Metodos--
 	
 	public AdministradorArticulosBean() {}
     
-    public String testEJB(){
-    	SolicitudArticuloDTO solicitudArticuloDTO = new SolicitudArticuloDTO();
-    	solicitudArticuloDTO.setEstadoSolicitud("pendiente");
-    	solicitudArticuloDTO.setIdSolicitud(453);
-    	solicitudArticuloDTO.setidDeposito(33);
-    	List<ItemSolicitudArticuloDTO> items = new ArrayList<ItemSolicitudArticuloDTO>();
-    	ItemSolicitudArticuloDTO itemSolicitudArticuloDTO = new ItemSolicitudArticuloDTO();
-    	ArticuloDTO articuloDTO = new ArticuloDTO();
-    	articuloDTO.setIdArticulo(655);
-    	articuloDTO.setDescripcion("un articulo para solicitar");
-    	itemSolicitudArticuloDTO.setArticulo(articuloDTO);
-    	itemSolicitudArticuloDTO.setCantidad(5);
-    	items.add(itemSolicitudArticuloDTO);
-    	solicitudArticuloDTO.setItems(items);
-    	clienteJmsParaDeposito.enviarSolicitudesArticulos(solicitudArticuloDTO);
-    	return "Test EJB OK !";
-    }
     private void guardarArticulo(Articulo articulo){
 		em.persist(articulo);
 	}
---
+++ ejbModule/despacho/ejb/AdministradorTestBean.java
@@ -0,0 +1,75 @@
+package despacho.ejb;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.ejb.Local;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import despacho.dominio.Portal;
+import despacho.ejb.interfaces.remotas.AdministradorTest;
+import despacho.ejb.interfaces.remotas.ClienteJmsParaDeposito;
+import despacho.ejb.interfaces.remotas.SimClienteRestParaDeposito;
+import despacho.ejb.interfaces.remotas.SimuladorLogisticaClienteJmsParaDespacho;
+import dto.ArticuloDTO;
+import dto.ItemSolicitudArticuloDTO;
+import dto.SolicitudArticuloDTO;
+
+@Stateless
+public class AdministradorTestBean implements AdministradorTest{
+
+    @PersistenceContext(unitName = "JPADB")
+    private EntityManager entityManager;
+    
+	@EJB
+	private ClienteJmsParaDeposito clienteJmsParaDeposito;
+	@EJB
+	private SimuladorLogisticaClienteJmsParaDespacho simuladorLogisticaClienteJmsParaDespacho;
+	@EJB
+	private SimClienteRestParaDeposito simClienteRestParaDeposito;
+    
+    public String testEJB(){
+    	return "Test EJB OK !";
+    }
+    
+    public String testEnviarSolicitudArticuloDepositoClienteJMS(){
+    	SolicitudArticuloDTO solicitudArticuloDTO = new SolicitudArticuloDTO();
+    	solicitudArticuloDTO.setEstadoSolicitud("pendiente");
+    	solicitudArticuloDTO.setIdSolicitud(453);
+    	solicitudArticuloDTO.setidDeposito(33);
+    	List<ItemSolicitudArticuloDTO> items = new ArrayList<ItemSolicitudArticuloDTO>();
+    	ItemSolicitudArticuloDTO itemSolicitudArticuloDTO = new ItemSolicitudArticuloDTO();
+    	ArticuloDTO articuloDTO = new ArticuloDTO();
+    	articuloDTO.setIdArticulo(655);
+    	articuloDTO.setDescripcion("un articulo para solicitar");
+    	itemSolicitudArticuloDTO.setArticulo(articuloDTO);
+    	itemSolicitudArticuloDTO.setCantidad(5);
+    	items.add(itemSolicitudArticuloDTO);
+    	solicitudArticuloDTO.setItems(items);
+    	clienteJmsParaDeposito.enviarSolicitudesArticulos(solicitudArticuloDTO);
+    	return "##TEST testEnviarSolicitudArticuloDepositoClienteJMS OK!";
+    }
+    
+    public String testRecibirOrdenDespachoPorWSDesdeLogistica(){
+    	return simuladorLogisticaClienteJmsParaDespacho.testRecibirOrdenDespachoPorWSDesdeLogistica();
+    }
+    
+    public String testRecibirArticulosPorRESTDesdeDeposito(){
+    	return simClienteRestParaDeposito.testRecibirArticulosPorRESTDesdeDeposito();
+    }
+
+    
+	@Override
+	public void persistirPortal(String descripcionPortal) {
+		// TODO Auto-generated method stub
+		Portal portal = new Portal();
+		portal.setDescripcion(descripcionPortal);
+		entityManager.persist(portal);
+		entityManager.flush();
+//		entityManager.close();
+	}
+
+}
